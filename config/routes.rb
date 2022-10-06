be0ce74f840876dfb467e5f75686447185280ced
m20187st <m20187st@takachiho.ac.jp>
2022-10-07 04:43:03 +0900

    add devise:controller user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "posts#index"
+
+  #  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   get 'posts/new', to: 'posts#new', as: :new_post
   post 'posts', to: 'posts#create'
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
   get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
   patch 'posts/:id', to: 'posts#update'
   delete 'posts/:id', to: 'posts#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
