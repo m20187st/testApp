0ca0f54d5a6187227c1eba66c375386551eeb0b6
m20187st <m20187st@takachiho.ac.jp>
2022-10-07 02:45:27 +0900

    init

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "posts#index"
+
+  get 'posts/new', to: 'posts#new', as: :new_post
+  post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#show', as: :post
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  patch 'posts/:id', to: 'posts#update'
+  delete 'posts/:id', to: 'posts#destroy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+end
