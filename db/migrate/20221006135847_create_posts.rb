d8fcbec66c7b840b554a2f69074c28ec868fbea0
m20187st <m20187st@takachiho.ac.jp>
2022-10-08 00:22:33 +0900

    edit new post controller

--- db/migrate/20221006135847_create_posts.rb
+++ db/migrate/20221006135847_create_posts.rb
@@ -1,9 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-		t.integer :user_id
+		t.integer :user_id, foreign_key: true
 		t.string :text
-		
 		t.timestamps
     end
   end
