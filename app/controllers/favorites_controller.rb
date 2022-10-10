dbef8751e5f38a3bf28a61ce6691316d814eff51
m20187st <m20187st@takachiho.ac.jp>
2022-10-10 22:19:33 +0900

    いいね！

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,20 @@
 class FavoritesController < ApplicationController
+  before_action :get_post
+
   def create
+	favorite = Favorite.new(user_id: current_user.id, post_id: @post.id)
+	favorite.save
+	render :new
   end
 
   def destroy
+	favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
+	favorite.destroy
+	render :new
+  end
+
+  private
+  def get_post
+	@post = Post.find_by(id: params[:post_id])
   end
 end
