577759d748ed87dd40ea38d4d1281ed2b3c19884
m20187st <m20187st@takachiho.ac.jp>
2022-10-11 21:50:13 +0900

    edit post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :favotite_users, through: :favorites, source: :user
+
+#  has_many :favotite_users, through: :favorites, source: :user
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+	favorites.exists?(user_id: user.id)
+  end
 end
