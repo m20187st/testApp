dbedb1213013c3c18f946d5583b08b395b4b3647
m20187st <m20187st@takachiho.ac.jp>
2022-10-11 23:37:57 +0900

    リダイレクト先をリクエストを送ったページに変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
   def create
 	favorite = Favorite.new(user_id: current_user.id, post_id: @post.id)
 	favorite.save
-	redirect_to @post
+	redirect_to request.referer
   end
 
   def destroy
 	favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
 	favorite.destroy
-	redirect_to @post
+	redirect_to request.referer
   end
 
   private
