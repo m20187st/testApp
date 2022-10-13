ddf6b86ac2edaacf5a61bfbe55e7e7c5e3342280
m20187st <m20187st@takachiho.ac.jp>
2022-10-13 20:57:36 +0900

    comment機能できた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
   def show
 	@post = Post.find(params[:id])
+	@comment = Comment.new
+	@comments = Comment.where(post_id: params[:id])
 	@favorited_sum = Favorite.where(post_id: params[:id]).count
   end
 
