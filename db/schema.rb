0320d96871ce6e36f99a57bcd95b77431a1a3ef5
m20187st <m20187st@takachiho.ac.jp>
2022-10-13 16:42:04 +0900

    comment db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_084633) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_073030) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index "\"usre_id\", \"post_id\"", name: "index_comments_on_usre_id_and_post_id", unique: true
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
@@ -41,6 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_084633) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "users"
 end
