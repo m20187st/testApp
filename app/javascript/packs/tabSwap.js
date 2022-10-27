e7a2cb491a8b142e356ead44feba0907383f546f
m20187st <m20187st@takachiho.ac.jp>
2022-10-27 18:44:01 +0900

    クラス名に -js を追加

--- app/javascript/packs/tabSwap.js
+++ app/javascript/packs/tabSwap.js
@@ -2,18 +2,18 @@ import jquery from "jquery"
 window.$ = jquery
 
 $(document).on('turbo:load', function() {
-	let tabs = $(".menu_item");
+	let tabs = $(".menu_item-js");
 	const tabsAry = Array.prototype.slice.call(tabs);
 	console.log(tabs);
 	console.log(tabsAry);
 
 	function tabSwitch() {
-		$(".active").removeClass("active");
-		$(this).addClass("active");
-		$(".show").removeClass("show");
+		$(".active-js").removeClass("active-js");
+		$(this).addClass("active-js");
+		$(".show-js").removeClass("show-js");
 
 		const index = tabs.index(this);
-		$(".content").removeClass("show").eq(index).addClass("show");
+		$(".content-js").removeClass("show-js").eq(index).addClass("show-js");
 	}
 
 	tabs.on('click', tabSwitch);
