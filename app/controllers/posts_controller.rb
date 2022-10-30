c0402b0ce262e70799013d60eecc0e3f5e8d1128
m20187st <m20187st@takachiho.ac.jp>
2022-10-31 03:38:27 +0900

    click to load moregit add .git add .git add .

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
   def index
-	@posts = Post.order(created_at: :desc).limit(10)
+	#@posts = Post.order(created_at: :desc).limit(10)
+	@pagy, @posts = pagy_countless(Post.order(created_at: :desc), items: 2)
 	if user_signed_in?
 		@favorite_posts = Post.includes(:favorites).where(favorites: {user_id: current_user.id}).order('favorites.created_at desc').limit(10)
 		@user_posts = Post.where(user_id: current_user.id).order(created_at: :desc).limit(10)
+	end
+	respond_to do |format|
+		format.html # GET
+		format.turbo_stream # POST
 	end
   end
 
