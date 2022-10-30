abf563e302c2069315fd8bfa23e04afc5f9d0199
m20187st <m20187st@takachiho.ac.jp>
2022-10-30 17:02:01 +0900

    Userにvalidates追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :posts, through: :favorites
   has_many :favotite_posts, through: :favorites, source: :post
   has_many :comments, dependent: :destroy
+
 end
